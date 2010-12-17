8ab34b509ec84bdb29d8542aec147e1070ab2593
tunnr <tunnerkrisztian@freemail.hu>
2010-12-17 19:31:32 +0100

    fix windows complie

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,11 +1,11 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define _REVISION "2"
- #define _HASH "2341191e4071"
+ #define _REVISION "*"
+ #define _HASH "*"
  #define _REVISION_DATE "*"
  #define _REVISION_TIME "*"
- #define FILEVER        0,0,2,0
- #define PRODUCTVER     0,0,2,0
- #define STRFILEVER     "0, 0, 2, 2341191e4071"
- #define STRPRODUCTVER  "0, 0, 2, 2341191e4071"
+ #define FILEVER        0,0,0,0
+ #define PRODUCTVER     0,0,0,0
+ #define STRFILEVER     "0, 0, *, *"
+ #define STRPRODUCTVER  "0, 0, *, *"
 #endif // __REVISION_H__
